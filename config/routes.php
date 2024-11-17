0496e1239942272159597f576d292087bc5d71bb
Shahin-Mia <shahin.cmt2@gmail.com>
2024-11-17 11:41:14 +0800

    refactor

--- config/routes.php
+++ config/routes.php
@@ -19,31 +19,31 @@ $app->get("/", function (Request $reqeuest, Response $response) {
     return $response;
 });
 
-$app->post("/api/create-users",         [Users::class, "create"]);
-$app->post("/api/account-activation",   [Users::class, "activateAccount"]);
-$app->post("/api/login",                [Users::class, "login"]);
-$app->post("/api/reset",                [Users::class, "sendMailToUser"]);
-$app->post("/api/reset-password",       [Users::class, "resetPassword"]);
-$app->get("/api/destinations",            [Destinations::class, "getAll"]);
-$app->get("/api/destinations/{id:[0-9]+}", [Destinations::class, "getById"]);
-
 $app->group("/api", function (RouteCollectorProxy $group) {
+    $group->post("/create-users",            [Users::class, "create"]);
+    $group->post("/account-activation",      [Users::class, "activateAccount"]);
+    $group->post("/login",                   [Users::class, "login"]);
+    $group->post("/reset",                   [Users::class, "sendMailToUser"]);
+    $group->post("/reset-password",          [Users::class, "resetPassword"]);
+    $group->get("/destinations",             [Destinations::class, "getAll"]);
+    $group->get("/destinations/{id:[0-9]+}", [Destinations::class, "getById"]);
 
-    $group->get("/users",               [Users::class, "getAllUsers"]);
     $group->group("", function (RouteCollectorProxy $group) {
-        $group->delete("/users/{id:[0-9]+}", [Users::class, "delete"]);
-    })->add(GetUser::class);
+        $group->get("/users",                    [Users::class, "getAllUsers"]);
+        $group->group("", function (RouteCollectorProxy $group) {
+            $group->delete("/users/{id:[0-9]+}", [Users::class, "delete"]);
+        })->add(GetUser::class);
 
+        $group->post("/destinations",               [Destinations::class, "create"]);
+        $group->post("/destinations/{id:[0-9]+}",   [Destinations::class, "update"]);
+        $group->delete("/destinations/{id:[0-9]+}", [Destinations::class, "delete"]);
 
-    $group->post("/destinations",               [Destinations::class, "create"]);
-    $group->post("/destinations/{id:[0-9]+}",   [Destinations::class, "update"]);
-    $group->delete("/destinations/{id:[0-9]+}", [Destinations::class, "delete"]);
-
-    $group->get("/products", ProductIndex::class);
-    $group->post("/products", [Products::class, "create"]);
-    $group->group("", function (RouteCollectorProxy $group) {
-        $group->get("/products/{id:[0-9]+}", [Products::class, "show"]);
-        $group->patch("/products/{id:[0-9]+}", [Products::class, "update"]);
-        $group->delete("/products/{id:[0-9]+}", [Products::class, "delete"]);
-    })->add(GetProduct::class);
-})->add(RequireApiKey::class);
+        $group->get("/products",   ProductIndex::class);
+        $group->post("/products",  [Products::class, "create"]);
+        $group->group("", function (RouteCollectorProxy $group) {
+            $group->get("/products/{id:[0-9]+}",    [Products::class, "show"]);
+            $group->patch("/products/{id:[0-9]+}",  [Products::class, "update"]);
+            $group->delete("/products/{id:[0-9]+}", [Products::class, "delete"]);
+        })->add(GetProduct::class);
+    })->add(RequireApiKey::class);
+});
